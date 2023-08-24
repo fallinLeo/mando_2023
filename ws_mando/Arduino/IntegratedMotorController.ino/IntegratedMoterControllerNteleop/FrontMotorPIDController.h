a05b474530dc4e1ab5dd010ce43aebb42a722211
fallinLeo <gogorabbit1@gmail.com>
2023-08-25 02:37:14 +0900

    08/25
    arduino update

---
+++ ws_mando/Arduino/IntegratedMotorController.ino/IntegratedMoterControllerNteleop/FrontMotorPIDController.h
@@ -0,0 +1,41 @@
+#ifndef FRONTMOTROPIDCONTROLLER_H
+#define FRONTMOTROPIDCONTROLLER_H
+
+#include "Arduino.h"
+
+class FrontMotorPIDController
+{
+  public: 
+    FrontMotorPIDController(uint8_t front_motor_pin_PWM, uint8_t front_motor_pin_ENA, uint8_t rear_motor_pin_PWM, uint8_t rear_motor_pin_ENA);
+    
+    int motor_pwm;
+    float y_m;
+
+    void set_gain(float Kp, float Ki, float Kd);
+    void control(float y_m, float r);
+    int get_pwm_by_PID(float y_m, float r);
+    
+  private:
+    uint8_t front_motor_pin_PWM;
+    uint8_t front_motor_pin_ENA;
+
+    uint8_t rear_motor_pin_PWM;
+    uint8_t rear_motor_pin_ENA;
+  
+    float Kp;
+    float Ki;
+    float Kd;
+  
+    bool enable_gain_setting;
+
+    float error;
+    float error_s;
+    float error_d;
+    float error_old;
+    float y_m_old;
+
+    void front_motor_control(int motor_pwm);
+    void rear_motor_control(int motor_pwm);
+};
+
+#endif
