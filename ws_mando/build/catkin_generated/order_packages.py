c4aacc6dee96b05a1acf2e43f2064eb24c493876
fallinLeo <gogorabbit1@gmail.com>
2023-08-28 20:23:21 +0900

    nothing
    nothing

--- ws_mando/build/catkin_generated/order_packages.py
+++ ws_mando/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/leo/Documents/GitHub/mando_2023/ws_mando/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/leo/ws_mando/devel;/opt/ros/noetic'.split(';') if '/home/leo/ws_mando/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
